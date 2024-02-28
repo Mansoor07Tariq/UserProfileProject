b74d24a83b0b74edfb5abf1c9e750717df7d2c73
Mansoor07Tariq <mansoor7tariq@gmail.com>
2024-02-29 01:21:02 +0500

    Dockerized Project

--- UserProfileProject/UserProfileProject/settings.py
+++ UserProfileProject/UserProfileProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&1akrsq2bcmg@okf5+#(#br1ypudufduc5co1v@s$@(ky1#kqg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '0.0.0.0']
 
 
 # Application definition
